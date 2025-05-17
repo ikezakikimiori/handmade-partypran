1e9afe2eeb8d8c57c58657f79cc2a1b3e8247c1e
ikezaki kiminori <115060680+ikezakikimiori@users.noreply.github.com>
2025-05-18 00:48:13 +0900

    js-gsap
    gsapでsection-titleにうごきを設置

--- js/main.js
+++ js/main.js
@@ -18,13 +18,14 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-// gsap
+// gsap--------------------------------
+// header
 gsap.from('header', {
   opacity: 0,
   delay: 0.5,
   duration: 2,
 });
-
+// title-top-view
 gsap.from('.main--title', {
   opacity: 0,
   delay: 0.5,
@@ -37,3 +38,67 @@ gsap.from('.title--area__main--viwe', {
   duration: 2,
 });
 
+// concept
+gsap.from('.concept-title', {
+  y:100,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: '.concept-title',
+    start: 'top 90%',
+    end: 'top 40%',
+    markers: true,
+  },
+});
+gsap.from('.concept-title__sub', {
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  scrollTrigger: {
+    trigger: '.concept-title',
+    start: 'top 90%',
+    end: 'top 40%',
+    markers: true,
+  },
+});
+
+// menu
+gsap.from('.menu-title', {
+   y:100,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.menu-title',
+    start: 'top 90%',
+    end: 'top 40%',
+    markers: true,
+  },
+});
+
+// information
+gsap.from('.information-title', {
+   y:100,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.information-title',
+    start: 'top 90%',
+    end: 'top 40%',
+    markers: true,
+  },
+});
+
+// access
+gsap.from('.access-title', {
+   y:100,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.access-title',
+    start: 'top 90%',
+    end: 'top 40%',
+    markers: true,
+  },
+});
+
+// footer
